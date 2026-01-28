9f0d8ca041e9418e5a9d6f70da980e9ed7c6e896
Yash Raj <yashraj1014@gmail.com>
2026-01-28 20:43:19 +0530

    Optimize Query

--- 02_joins/9_Join_&_cte.sql
+++ 02_joins/9_Join_&_cte.sql
@@ -30,6 +30,31 @@ LEFT join main_cte t2
     ON t1.user_id = t2.user_id
 GROUP BY t1.user_id
 
+
 -- Optimized SQL Query:
+WITH main_cte AS (
+    SELECT 
+        user_id,
+        COUNT(*) AS total_actions,
+        SUM(action = 'confirmed') AS confirmed_actions
+    FROM Confirmations
+    GROUP BY user_id
+    )
+
+SELECT 
+    t1.user_id,
+    ROUND(IFNULL(confirmed_actions/IFNULL(total_actions, 0), 0), 0) AS confirmation_rate
+FROM Signups t1
+LEFT JOIN main_cte t2
+    ON t1.user_id = t2.user_id
+
+
 
 -- Optimized SQL Query without CTE:
+SELECT 
+    s.user_id,
+    ROUND(IFNULL(SUM(c.action = 'confirmed')/COUNT(c.action), 0), 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id
