49b40532fe2f0d0a4e1adca2768deb939446a1b4
Yash Raj <yashraj1014@gmail.com>
2026-02-09 22:31:33 +0530

    Practice agg

---
+++ 03_aggregations/5_derived_formula.sql
@@ -0,0 +1,20 @@
+-- Problem:
+
+-- Dataset:
+
+-- Approach:
+-- 1.
+-- 2.
+-- 3.
+
+-- SQL Query:
+SELECT 
+    query_name, 
+    ROUND(AVG(rating/`position`), 2) AS quality,
+    ROUND(
+        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)/COUNT(*)*100,
+        2)
+    AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
+
