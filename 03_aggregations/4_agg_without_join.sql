8cd06cf386f6a2cbd9a15601a7c11b935c3287b4
Yash Raj <yashraj1014@gmail.com>
2026-02-10 22:20:02 +0530

    update query

--- 03_aggregations/4_agg_without_join.sql
+++ 03_aggregations/4_agg_without_join.sql
@@ -5,13 +5,23 @@
 -- Dataset: Users table with columns: user_id, user_name
 --          Register table with columns: contest_id, user_id
 
--- Approach:
--- 1.
--- 2.
--- 3.
+-- Approach: 
+-- 1. We will group by contest_id first from the Register table since we want info about each contest.
+-- 2. Now, in order to calculate the percentage, we need to take info from both the tables without joing them. 
+--    If we join we won't be able to get the total number of user_id as in the merged table,
+--    we will have same number of user_id as in the Register table because we are making groups per contest_id. 
+
+-- 3. So, to get the total number of users, we can use a subquery that counts the number of entries in the Users table.
+-- 4. Finally, we will order the result by percentage in descending order and by contest_id in ascending order in case of ties.
 
 -- SQL Query:
-SELECT contest_id, ROUND((COUNT(*)/(SELECT COUNT(*)FROM users))*100, 2) AS percentage
+SELECT 
+    contest_id, 
+    ROUND(
+        (COUNT(*)/(SELECT COUNT(*) FROM users))*100,
+    2) AS percentage
 FROM Register
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id ASC;
+
+-- Python Query:
